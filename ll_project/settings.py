7fcfd6bd929131dd5e7c03bf7467d2aff9fc1fee
Aryagohar <157883785+aryagohar@users.noreply.github.com>
2026-07-06 21:00:37 +0330

    Add STATIC_ROOT configuration for static files

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Email
 # https://docs.djangoproject.com/en/dev/topics/email/#topic-email-configuration
